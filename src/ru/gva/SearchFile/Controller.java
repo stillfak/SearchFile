77c9e814d1bea3bbbcd92389ad836b7de7d49a15
vadim <vadim.gavrikov.1998@gmail.com>
2018-05-05 13:13:24 +0300

    Изменен тип элементов arrayList возвращаемого результата из мето

--- src/ru/gva/SearchFile/Controller.java
+++ src/ru/gva/SearchFile/Controller.java
@@ -1,19 +1,7 @@
 package ru.gva.SearchFile;
 
-import java.awt.*;
-import java.io.File;
-
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -21,16 +9,23 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Класс отлеживает действие пользователя с графическим интерфейсом.
+ *
+ * @author Gavrikov V. 15it18
+ */
 public class Controller {
-    File startDirectory;
+    private File startDirectory;
 
-    @FXML
-    private ResourceBundle resources;
+    //@FXML
+    //private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+    //@FXML
+    //private URL location;
 
     @FXML
     private TextField searchMsg;
@@ -53,33 +48,11 @@ public class Controller {
             directory.setTitle("Select directory for changeStartDir");
             startDirectory = new File(String.valueOf(directory.showDialog(new Stage())));
 
-            if (startDirectory.getAbsolutePath() != null) {
+//            startDirectory.getAbsolutePath();
 
-                path.setText(startDirectory.getAbsolutePath());
-            } else {
-                path.setText("");
-            }
+            path.setText(startDirectory.getAbsolutePath());
 
         });
-
-//        Stage st = FXMLLoader.load(location);
-//        Stage resultStage =
-//        Stage resultStage = FXMLLoader.load(location,resources);
-
-//        search.setOnAction(event -> {
-//            FXMLLoader fxmlLoader = new FXMLLoader();
-//            Scene resultScene = null;
-//            try {
-//                resultScene = new Scene(FXMLLoader.load(getClass().getResource("/ru/gva/SearchFile/res/resultSearch.fxml")));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-
-
-//                Main.resultStage.setScene(new Scene(stageResultSearch));
-//                Main.resultStage.show();
-//            st.setScene(stageResultSearch);
-//            resultStage.setScene(new Scene(stageResultSearch));
 
 
     }
@@ -99,9 +72,9 @@ public class Controller {
         Stage resultStage = new Stage();
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.TOP_CENTER);
-        ArrayList<Label> result = searchFiles(new File(path.getText()), String.valueOf(searchMsg.getCharacters()));
+        ArrayList<String> result = searchFiles(new File(path.getText()), String.valueOf(searchMsg.getCharacters()));
         int y=0;
-        for (Label label : result) {
+        for (String label : result) {
 //            button.setOnAction(event -> {
 //                Desktop desktop = null;
 //                if (desktop.isDesktopSupported()) {
@@ -113,8 +86,8 @@ public class Controller {
 //                    }
 //                }
 //            });
-            gridPane.getChildren().addAll(label);
-            System.out.println(label.getText());
+            gridPane.getChildren().addAll(new Label(label));
+//            System.out.println(label.getText());
             gridPane.setVgap(y);
             y=+20;
 
@@ -124,11 +97,7 @@ public class Controller {
         resultStage.show();
 
 
-//        resultController.setTitle("Результат");
-//        resultController.setScene(resultScene);
-//        resultController.setAlwaysOnTop(true);
-//        resultController.show();
-//        resultController.initialize();
+
 
     }
 
@@ -138,15 +107,16 @@ public class Controller {
      * @param pathDirectory дирректория для поиска
      * @param searchMsg     запрос пользователя
      */
-    private static ArrayList searchFiles(File pathDirectory, String searchMsg) throws NullPointerException {
+    public static ArrayList<String> searchFiles(File pathDirectory, String searchMsg) throws NullPointerException {
         File[] files = pathDirectory.listFiles();
         assert files != null;
-        ArrayList<Label> resultTrueFile = new ArrayList<>();
-
+        ArrayList<String> resultTrueFile = new ArrayList<>();
+//        Label addInArray = new Label();
         for (File file : files) {
             if (file.getName().matches(searchMsg + ".{0,4}")) {
-
-                resultTrueFile.add(new Label(file.getAbsolutePath()));
+//                addInArray = new Label(file.getAbsolutePath());
+//                assert addInArray != null;
+                resultTrueFile.add(file.getAbsolutePath());
 
 //                resultTrueFile.get(resultTrueFile.size()-1).setText("Открыть");
             }
@@ -158,10 +128,7 @@ public class Controller {
 
     }
 
-//    public static void main(String[] args) {
 
-//
-//    }
 
 
 }
