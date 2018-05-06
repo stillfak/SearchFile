d1f1511c2ff8d7d7bfb3b4c5b78ee163dda605c4
vadim <vadim.gavrikov.1998@gmail.com>
2018-05-06 19:29:54 +0300

    обвлен класс unit test

--- src/ru/gva/SearchFile/test/ControllerTest.java
+++ src/ru/gva/SearchFile/test/ControllerTest.java
@@ -1,23 +1,24 @@
 package ru.gva.SearchFile.test;
 
 import org.junit.Test;
-import ru.gva.SearchFile.Controller;
 
 import java.io.File;
 
 import static org.junit.Assert.assertEquals;
+import static ru.gva.SearchFile.SearchFiles.resultTrueFile;
+import static ru.gva.SearchFile.SearchFiles.searchFiles;
 
 public class ControllerTest {
 
     @Test
     public void initialize() {
 //        File expected = new File("/home/vadim/Документы/SearchInFile");
-
 //        assertEquals(expected, new File("/home/vadim/Документы/SearchInFile"));
     }
 
     @Test
     public void start() {
-        assertEquals(Controller.searchFiles(new File("/home/vadim/Документы/SearchInFile"),"src").get(0),"/home/vadim/Документы/SearchInFile/src");
+        searchFiles(new File("/home/vadim/Документы/SearchInFile"),"src");
+        assertEquals(resultTrueFile.get(0),"/home/vadim/Документы/SearchInFile/src");
     }
 }
